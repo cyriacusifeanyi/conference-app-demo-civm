25fa6c4f3f4c6883f7b8069625af8268f1f3748f
cyriacusifeanyi <cyriacusifeanyi@gmail.com>
2020-04-30 15:45:57 +0100

    1. I changed the pom.xml configuration, so that it now package app as a .war file which will be used on an external containers like Apache Tomcat 2. At the entry point of the app (HospitalManagementSystemApplication declaration), I extended the SpringBootServletInitializer class 3. Because the {DB_URL} variable in the application.properties file was not visible during packaging i had to hard-code the URL (builder.url("...."))  in the config/PersistenceConfiguration.java
    After running on the Apache Tomcat server the homepage ran error free (because it de=oesnt require database connection) while the other API endpoints gave
    error 500 (the server needed postgresql connector).

--- src/main/java/com/vgg/hospitalmanagementsystem/HospitalManagementSystemApplication.java
+++ src/main/java/com/vgg/hospitalmanagementsystem/HospitalManagementSystemApplication.java
@@ -2,9 +2,10 @@ package com.vgg.hospitalmanagementsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HospitalManagementSystemApplication {
+public class HospitalManagementSystemApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HospitalManagementSystemApplication.class, args);
--- src/main/java/com/vgg/hospitalmanagementsystem/controllers/config/PersistenceConfiguration.java
+++ src/main/java/com/vgg/hospitalmanagementsystem/controllers/config/PersistenceConfiguration.java
@@ -1,20 +1,24 @@
 package com.vgg.hospitalmanagementsystem.controllers.config;
 
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceConfiguration {
-//    @Bean
-//    public DataSource dataSource(){
-//        DataSourceBuilder builder = DataSourceBuilder.create();
-//        builder.url("jdbc:postgresql://localhost:5432/conference_app");
-//
-////        we can overwrite settings in application.properties or application.yml here.
-////        It gave some error, so I added the below lines of code
+    @Bean
+    public DataSource dataSource(){
+        DataSourceBuilder builder = DataSourceBuilder.create();
+        builder.url("jdbc:postgresql://localhost:5432/conference_app");
+
+//        we can overwrite settings in application.properties or application.yml here.
+//        It gave some error, so I added the below lines of code
 //        builder.username("postgres");
 //        builder.password("zx12cv36");
-//
-//        System.out.println("My custom datasource bean has been initialized and set");
-//        return builder.build();
-//    }
+
+        System.out.println("My custom datasource bean has been initialized and set");
+        return builder.build();
+    }
 }
