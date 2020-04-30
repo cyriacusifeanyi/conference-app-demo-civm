e4b15819348aaf0d7bde07f9dea0f81290aea156
cyriacusifeanyi <cyriacusifeanyi@gmail.com>
2020-04-30 16:22:56 +0100

    i have reveted the last change (output is in .jar executable), and also removed all hard coded values - as i was able to pass them through as environment vairable in a custom configuration of maven:package command.

--- src/main/java/com/vgg/hospitalmanagementsystem/HospitalManagementSystemApplication.java
+++ src/main/java/com/vgg/hospitalmanagementsystem/HospitalManagementSystemApplication.java
@@ -2,10 +2,9 @@ package com.vgg.hospitalmanagementsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HospitalManagementSystemApplication extends SpringBootServletInitializer {
+public class HospitalManagementSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HospitalManagementSystemApplication.class, args);
